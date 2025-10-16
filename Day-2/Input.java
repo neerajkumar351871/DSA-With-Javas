222885d359a2bf9d485657e25cce5d4730765f14
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-10-17 00:16:11 +0530

    Day -2  complete

---
+++ Day-2/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+    public class Input {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        String name=sc.next();
+        System.out.println("Your Entered number  is :" + number);
+        System.out.println("Your Entered n String  is :" +name);
+
+    }
+}
