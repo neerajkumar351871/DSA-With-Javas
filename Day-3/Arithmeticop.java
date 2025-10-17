e60f372454991d24052a26f9458b3711806cc05b
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-10-18 00:15:06 +0530

    Operator complete

---
+++ Day-3/Arithmeticop.java
@@ -0,0 +1,16 @@
+public  class Arithmeticop{
+    public static void main(String[] args) {
+          int a = 10;
+        int b = 30;
+        int c = 39;
+        System.out.println("Addation:" + (a + b));
+        System.out.println("Subtraction:" + (a - b));
+        System.out.println("Multiple:" + (a * b));
+        System.out.println("Divide:" + (b / a));//Give the quotient.
+        System.out.println("Modulo:" + (c % a));//GIve the Remainder
+        System.out.println("unary increment operator pre:" + (++a));
+        System.out.println("unary increment operator post:" + (a++));
+        System.out.println("unary decrement operator:" + (a--));
+        System.out.println("unary decrement operator:" + (--a));
+    }
+}
---
+++ Day-3/Logicalop.java
@@ -0,0 +1,11 @@
+
+public class Logicalop {
+
+    public static void main(String[] args) {
+        System.out.println((10 > 2) && (4 > 2));// Logical operator and && Ans = True
+        System.out.println((10 > 2) && (4 < 2));// Logical operator and && Ans = False
+        System.out.println((10 > 2) || (4 < 2));// Logical operator OR || Ans = True
+        System.out.println((10 < 2) || (4 < 2));// Logical operator OR || Ans = False
+        System.out.println((20)!=(10));
+    }
+}
