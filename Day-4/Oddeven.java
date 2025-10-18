4a9500b16e2534222d8205ea964394d2af035dbe
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-10-18 11:40:57 +0530

    Odd Even

---
+++ Day-4/Oddeven.java
@@ -0,0 +1,13 @@
+public  class Oddeven
+{
+     public static void main(String[] args) {
+        int number =1;
+        if(number%2==1)
+        {
+         System.out.println("Number is odd:"+number);
+         }
+        else{
+         System.out.println("Number is Even:"+number);
+         }
+     }
+}
