bebdb47bef9f9261901190beed535ceffd11701b
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-10-23 22:45:16 +0530

    While loop

---
+++ Day-5/Ternaryop.java
@@ -0,0 +1,8 @@
+public class Ternaryop
+{
+    public static void main(String args[]) 
+    {
+  int number = 10;
+    String value = ((number % 2) == 0) ? 'Even' : 'odd';
+    System.out.print("Number is " + value);    }
+}
---
+++ Day-5/Whileloop.java
@@ -0,0 +1,11 @@
+
+public class Whileloop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("Hellow"+i);
+            i++;
+        }
+    }
+}
