2b3b8c33ac6291c73fc8e023c60827f4f31da17b
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-11-01 10:36:27 +0530

    Function

---
+++ Day-2/Function.java
@@ -0,0 +1,11 @@
+public  class  Function
+{
+    public static void  printhello()
+    {
+        System.out.println("Hello");
+    }
+ public static  void main(String args[])
+ {
+    printhello();
+ }
+}
