e1f0dd6ea98b007aa35a3fc03dc70b8416ad28cd
neerajkumar351871 <neerajkumar351871@gmail.com>
2025-11-05 22:53:21 +0530

    halfpyramid

---
+++ Day-3/halfpyramid.java
@@ -0,0 +1,22 @@
+
+public class halfpyramid {
+    public static void halfpyramid() {
+        int n = 4;
+        //outer loop
+        for (int i = 1; i <= n; i++) {
+            //space
+            for (int space = 1; space <= n - i; space++) {
+                System.out.print(" ");
+            }
+            //Star
+            for (int str = 1; str <= i; str++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        halfpyramid();
+    }
+}
